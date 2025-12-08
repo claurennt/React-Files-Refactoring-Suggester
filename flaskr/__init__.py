84b18c200e39aad4e2578268b46358b14c683854
claurennt <c.benedetti1989@gmail.com>
2025-12-08 18:00:30 +0100

    feat: add templates, adjust route return

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,17 +1,21 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app():
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+
+    app = Flask(__name__)
+
     app.config.from_mapping(
         SECRET_KEY="dev",
     )
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
+    @app.route("/")
+    def home():
+        return render_template("file_upload.html")
 
     return app
+
+
+app = create_app()
